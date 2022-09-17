103e45d3cc0c718930a7ecc0de3a5fee75d20b17
abdelghafour aouad <abdelghafour.aouad0@gmail.com>
2022-09-17 20:52:56 +0100

    First version - add produit

--- project.c
+++ project.c
@@ -2,6 +2,9 @@
 #include <conio.h>
 #include <stdlib.h>
 
+
+int count=0;
+
 // structers
 
 struct produit{
@@ -13,80 +16,116 @@ struct produit{
 // functions
 
 void lineBreak(){
-    printf("\n*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*\n");
+    printf("\n*-----------------------------------------*\n");
 }
 void cleanConsole(){
     system("cls");
 }
-
-void ajouterProduit(struct produit* allProduit,int *count){
-cleanConsole();
-lineBreak();
-printf("\nCode de produit : ");
-scanf("%s",&allProduit[*count].code);
-
-
-printf("\nNom de produit : ");
-scanf("%s",&allProduit[*count].nom);
+void ajouterProduits(struct produit* allProduit, int x){
+    int number;
+    printf("Combien des produit : ");
+    scanf("%d",&number);
+    for(int i=0; i<number;i++){
+       ajouterProduit(allProduit,count,i);
+        }
+    }
 
 
-printf("\nQuantite de produit : ");
-scanf("%d",&allProduit[*count].quantite);
+    void ajouterProduit(struct produit* allProduit,int i){
+    cleanConsole();
+    lineBreak();
+    if(i>-1){
+        printf("*-----------Produit number : %d -----------*",i+1);
+        lineBreak();
+    }else{
+        printf("*-------------Ajouter Produit-------------*");
+        lineBreak();
+
+    }
+    printf("\nCode de produit : ");
+    scanf("%s",&allProduit[count].code);
 
 
-printf("\nPrix de produit : ");
-scanf("%f",&allProduit[*count].prix);
+    printf("\nNom de produit : ");
+    scanf("%s",&allProduit[count].nom);
 
-cleanConsole();
-lineBreak();
-printf("Le produit etait ajouter avec succes !!");
-lineBreak();
-printf("%s\n",allProduit[*count].code);
-printf("%s\n",allProduit[*count].nom);
-printf("%d\n",allProduit[*count].quantite);
-printf("%lf",allProduit[*count].prix);
-count++;
-lineBreak();
-sleep(4);
-cleanConsole();
+
+    printf("\nQuantite de produit : ");
+    scanf("%d",&allProduit[count].quantite);
+
+
+    printf("\nPrix de produit : ");
+    scanf("%f",&allProduit[count].prix);
+
+    cleanConsole();
+    lineBreak();
+    printf("Le produit etait ajouter avec succes !!");
+    lineBreak();
+    printf("%s\n",allProduit[count].code);
+    printf("%s\n",allProduit[count].nom);
+    printf("%d\n",allProduit[count].quantite);
+    printf("%lf",allProduit[count].prix);
+    count++;
+    lineBreak();
+    sleep(4);
+    cleanConsole();
+}
+void listerProduits(struct produit* allProduit){
+    //printf("%d",count);
+    //sleep(3);
+    if(count==0){
+        lineBreak();
+        printf("*--------------Aucun Produit -------------*");
+        lineBreak();
+        sleep(4);
+        cleanConsole();
+    }else{
+        for(int i=0;i<count;i++){
+            lineBreak();
+            printf("*-----------Produit number : %d -----------*",i+1);
+            lineBreak();
+            printf("Le code de produit: %s\n",allProduit[i].code);
+            printf("Le nom de produit: %s\n",allProduit[i].nom);
+            printf("La quantite de produit: %d\n",allProduit[i].quantite);
+            printf("Le prix de produit: %lf\n",allProduit[i].prix);
+        }
+
+    lineBreak();
+    lineBreak();
+    sleep(4);
+
 }
 
-void case2(){
-cleanConsole();
-lineBreak();
-printf("\n hello2 \n");
-lineBreak();
-}
 void defaultt(){
-cleanConsole();
-lineBreak();
-printf("\n default \n");
-lineBreak();
+    cleanConsole();
+    lineBreak();
+    printf("\n default \n");
+    lineBreak();
 }
 
 void main(){
     struct produit allProduit[100];
-int count=0, a;
+    int  a;
 
     do{
-   //     cleanConsole();
         printf("choisir:\n");
-
-        printf("1- ajouter un seul produit .\n");
-        printf("2- plusieur produit .\n");
-        printf("3- plusieur produit .\n");
+        printf("\t1- Ajouter un seul produit .\n");
+        printf("\t2- Plusieur produit .\n");
+        printf("\t3- Lister tout les produits .\n");
         scanf("%d",&a);
         switch (a)
         {
             case 1:
-              ajouterProduit(&allProduit,&count);
-              break;
-
+                ajouterProduit(&allProduit, -1);
+                break;
             case 2:
-              case2();
-              break;
+                ajouterProduits(&allProduit,  -1);
+                break;
+            case 3:
+                listerProduits(&allProduit);
+                break;
             default:
-             defaultt();
+                defaultt();
         }
     }while(1);
 }
